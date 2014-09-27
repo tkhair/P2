7a47e4e2268c46c9afd094881e4fb191b5a9f473
Tatiana Khair <t.mirkevich@gmail.com>
2014-09-27 22:46:21 +0400

    layout preparation (added bootstrap and general layout)

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CSCI E-15 - Tatiana Khair</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+    <h1>Tatiana Khair - CSCI E-15 P2 - Password Generator</h1>
+
+	
+
+
+<div class="footer">
+    <p>(c) Tatiana Khair 2014<br/>
+    This page have been developed in-line with Assignment 2 requirements of CSCI E-15 class at Harvard Extension School</p>
+</div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
